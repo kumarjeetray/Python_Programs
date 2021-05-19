b45635fb1b03b7452bdd0f30b634e611e54fd20e
Kumarjeet Ray <53223216+kumarjeetray@users.noreply.github.com>
2021-05-19 10:31:18 +0530

    Add files via upload

---
+++ open_browser_music.py
@@ -0,0 +1,8 @@
+'''
+To open a page in a web browser recursively
+'''
+import webbrowser
+import time
+for i in range(0,3):
+    webbrowser.open('https://www.jiosaavn.com/my-music/playlists')
+    time.sleep(90)
