98471ff74e26cfef25e1973faf03592e91a549df
Kumarjeet Ray <53223216+kumarjeetray@users.noreply.github.com>
2021-06-17 00:17:01 +0530

    Add files via upload

---
+++ Socket Programming/SERVER2.py
@@ -0,0 +1,10 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((socket.gethostname(), 1025))
+s.listen(5)
+while True:
+    clt_soc, clt_add = s.accept()
+    print(f"Connection to {clt_add} established")
+    clt_soc.send(bytes("Socket Programming", "utf-8"))
+    clt_soc.close()
