8bd99fb8372bb92b40e3bd111eeb400257e0c1db
Kumarjeet Ray <53223216+kumarjeetray@users.noreply.github.com>
2021-06-17 00:16:40 +0530

    Add files via upload

---
+++ Socket Programming/CLIENT2.py
@@ -0,0 +1,10 @@
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((socket.gethostname(), 1025))
+complete_msg = ''
+while True:
+    msg = s.recv(8)
+    if len(msg) <= 0:
+        break
+    complete_msg += msg.decode("utf-8")
+print(complete_msg)
