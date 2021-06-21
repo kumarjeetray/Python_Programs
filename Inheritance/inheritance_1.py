e5a7bc0f9c5a934d5822dac363aa92e0f05ffbb5
Kumarjeet Ray <53223216+kumarjeetray@users.noreply.github.com>
2021-06-21 08:06:54 +0530

    Add files via upload

---
+++ Inheritance/inheritance_1.py
@@ -0,0 +1,7 @@
+#from Chef import Chef
+from ChineseChef import ChineseChef
+#myChef = Chef()
+#myChef.make_chicken()
+myChineseChef = ChineseChef()
+myChineseChef.make_fried_rice()
+myChineseChef.make_special_dish()
