5e10980235304ae0c8e9b543bee2d1296bd3637d
Kumarjeet Ray <53223216+kumarjeetray@users.noreply.github.com>
2021-06-21 08:13:24 +0530

    Add files via upload

---
+++ Codes/story.py
@@ -0,0 +1,6 @@
+char_name="John"
+char_age="35"
+print("There was once a man named "+char_name+",")
+print("he was "+ char_age +" years old. ")
+print("He really liked the name "+char_name+",")
+print("but he didn't like being "+ char_age +".")
