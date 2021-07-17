f8e50ab5feeeea80129d7623e9542c6a08679551
Kumarjeet Ray <53223216+kumarjeetray@users.noreply.github.com>
2021-07-17 09:03:10 +0530

    Add files via upload

---
+++ Codes/AAP Lab/1(d).py
@@ -0,0 +1,11 @@
+# Name: Kumarjeet Ray
+# Roll Number:1928170
+# Batch: CSSE 3
+
+
+if __name__ == '__main__':
+    sen = 'I am learning Python everyday'
+    if sen.find('Everyday') == -1:
+        print("Everyday is not present")
+    else:
+        print("Everyday is present")
