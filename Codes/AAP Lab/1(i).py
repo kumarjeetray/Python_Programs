6f88ab676e78cb1629096b7de644ba90199f3dc2
Kumarjeet Ray <53223216+kumarjeetray@users.noreply.github.com>
2021-07-18 08:04:56 +0530

    Add files via upload

---
+++ Codes/AAP Lab/1(i).py
@@ -0,0 +1,10 @@
+# Name: Kumarjeet Ray
+# Roll Number:1928170
+# Batch: CSSE 3
+
+
+if __name__ == '__main__':
+    sen = 'I am learning Python everyday'
+    starting = sen.find('Python')
+    ending = len('Python')
+    print(sen[starting:starting + ending])
