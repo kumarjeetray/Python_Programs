a383f253a3189b2c939d3ebd69acad25d89a278a
Kumarjeet Ray <53223216+kumarjeetray@users.noreply.github.com>
2021-07-19 09:10:10 +0530

    Add files via upload

---
+++ Codes/AAP Lab/3.py
@@ -0,0 +1,9 @@
+# Name: Kumarjeet Ray
+# Roll Number:1928170
+# Batch: CSSE 3
+
+
+if __name__ == '__main__':
+    num1 = eval(input("Enter a number:"))
+    num2 = eval(input("Enter another number:"))
+    print("Sum = " + str(num1 + num2))
