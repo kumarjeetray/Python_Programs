b9c0331bbc74307a5778adf1f1fb3e5bcec89d51
Kumarjeet Ray <53223216+kumarjeetray@users.noreply.github.com>
2021-07-19 09:09:48 +0530

    Add files via upload

---
+++ Codes/AAP Lab/2.py
@@ -0,0 +1,13 @@
+# Name: Kumarjeet Ray
+# Roll Number:1928170
+# Batch: CSSE 3
+
+
+if __name__ == '__main__':
+    num1 = 9
+    num2 = 9.231
+    word = 'Python'
+    print('Int to Float: ' + str(float(num1)))
+    print('Int to String: ' + str(num1))
+    print('Float to Int: ' + str(int(num1)))
+    print('Float to String: ' + str(num1))
