3c5799db0a3703e4061041ac06f2c9c511fa6c2d
Kumarjeet Ray <53223216+kumarjeetray@users.noreply.github.com>
2021-07-25 08:45:31 +0530

    Add files via upload

---
+++ Codes/AAP Lab/Lab 2/1(d).py
@@ -0,0 +1,10 @@
+# Name - Kumarjeet Ray
+# Roll - 1928170
+# Batch - CSSE 3
+
+
+if __name__ == '__main__':
+    num = input("Enter a number:")
+    print("Before conversion:",type(num))
+    converted_num = int(num)
+    print("After conversion:",type(converted_num))
