e9d79c845faf271d44a365ea9d9da86be99a8e23
Kumarjeet Ray <53223216+kumarjeetray@users.noreply.github.com>
2021-07-31 11:31:19 +0530

    Create 1(a).py

---
+++ Codes/AAP Lab/Lab 3/1(a).py
@@ -0,0 +1,11 @@
+# Name - Kumarjeet Ray
+# Roll Number - 1928170
+# Batch - CSSE3
+if __name__ == '__main__':
+    cars = ['Ford', 'BMW', 'Volvo']
+    cars_temp = cars
+    print("List:", cars)
+    cars.sort()
+    print("After sorting in ascending order:", cars)
+    cars_temp.sort(reverse=True)
+    print("After sorting in descending order:", cars_temp)
