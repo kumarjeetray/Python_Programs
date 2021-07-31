69baaf2ca93d36de4bb010b590ed2a16192d3e5f
Kumarjeet Ray <53223216+kumarjeetray@users.noreply.github.com>
2021-07-31 11:32:42 +0530

    Add files via upload

---
+++ Codes/AAP Lab/Lab 3/1(d).py
@@ -0,0 +1,8 @@
+# Name - Kumarjeet Ray
+# Roll Number - 1928170
+# Batch - CSSE3
+if __name__ == '__main__':
+    for i in range(1, 3):
+        for j in range(i*3):
+            print('X',end="")
+        print(end="\n")
