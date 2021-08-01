bd2c4dff2e99397ce4d55554952e054770e46d46
Kumarjeet Ray <53223216+kumarjeetray@users.noreply.github.com>
2021-08-01 08:08:39 +0530

    Add files via upload

---
+++ Codes/AAP Lab/Lab 3/1(e).py
@@ -0,0 +1,9 @@
+# Name - Kumarjeet Ray
+# Roll Number - 1928170
+# Batch - CSSE3
+if __name__ == '__main__':
+    cars = ['Ford', 'BMW', 'Volvo','tata', 'FERRARI', 'hyundai']
+    cars_temp = cars
+    print("List:", cars)
+    cars.sort()
+    print("After sorting in ascending order:", cars)
