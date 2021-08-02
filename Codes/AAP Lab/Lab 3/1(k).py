6f44c388d000bca3a9877d4f77d6a65aa3534791
Kumarjeet Ray <53223216+kumarjeetray@users.noreply.github.com>
2021-08-02 10:14:25 +0530

    Add files via upload

---
+++ Codes/AAP Lab/Lab 3/1(k).py
@@ -0,0 +1,7 @@
+# Name - Kumarjeet Ray
+# Roll Number - 1928170
+# Batch - CSSE3
+if __name__ == '__main__':
+    cars = ['Ford', 'BMW', 'Volvo','tata', 'FERRARI', 'hyundai']
+    print('Original List:', cars)
+    del cars
