24b7c8c8af1a88888312eac53164ba75c727a124
Kumarjeet Ray <53223216+kumarjeetray@users.noreply.github.com>
2021-08-02 10:14:04 +0530

    Add files via upload

---
+++ Codes/AAP Lab/Lab 3/1(j).py
@@ -0,0 +1,8 @@
+# Name - Kumarjeet Ray
+# Roll Number - 1928170
+# Batch - CSSE3
+if __name__ == '__main__':
+    cars = ['Ford', 'BMW', 'Volvo','tata', 'FERRARI', 'hyundai']
+    print('Original List:', cars)
+    cars.clear()
+    print('After Clearing:', cars)
