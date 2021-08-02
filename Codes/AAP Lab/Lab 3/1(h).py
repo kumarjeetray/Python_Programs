f2db06233f9dbe3535d066a34ed77db8339c8684
Kumarjeet Ray <53223216+kumarjeetray@users.noreply.github.com>
2021-08-02 10:13:27 +0530

    Add files via upload

---
+++ Codes/AAP Lab/Lab 3/1(h).py
@@ -0,0 +1,10 @@
+# Name - Kumarjeet Ray
+# Roll Number - 1928170
+# Batch - CSSE3
+if __name__ == '__main__':
+    cars = ['Ford', 'BMW', 'Volvo','tata', 'FERRARI', 'hyundai']
+    print('Original List:', cars)
+    cars.pop()
+    print('After Pop:', cars)
+    cars.remove('BMW')
+    print('After Remove:', cars)
