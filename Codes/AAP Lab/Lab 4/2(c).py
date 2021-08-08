b6f58016632eabac917c5d064d02ae457aefd922
Kumarjeet Ray <53223216+kumarjeetray@users.noreply.github.com>
2021-08-08 08:59:13 +0530

    Add files via upload

---
+++ Codes/AAP Lab/Lab 4/2(c).py
@@ -0,0 +1,8 @@
+# Name - Kumarjeet Ray
+# Roll Number - 1928170
+# Batch - CSSE3
+if __name__ == '__main__':
+    cars = ('Ford', 'BMW', 'Volvo')
+    print('Original Tuple:', cars)
+    F,B,V = cars
+    print(F,B,V)
