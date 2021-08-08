2015daf572f32991fac8a65fe587be787662901f
Kumarjeet Ray <53223216+kumarjeetray@users.noreply.github.com>
2021-08-08 08:59:39 +0530

    Add files via upload

---
+++ Codes/AAP Lab/Lab 4/2(d).py
@@ -0,0 +1,7 @@
+# Name - Kumarjeet Ray
+# Roll Number - 1928170
+# Batch - CSSE3
+if __name__ == '__main__':
+    cars = ('Ford', 'BMW', 'Volvo', 'tata', 'FERRARI', 'hyundai')
+    F,B,V,*car = cars
+    print(F,B,V, car)
